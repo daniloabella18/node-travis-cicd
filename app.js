67044034f141868c812a011fa7c29cce652c1438
daniloabella18 <daniloabella@gmail.com>
2022-07-09 23:53:41 -0400

    travis

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+require('dotenv').config(); // Permite usar variables .env
 
+const express = require('express')
+const cors = require('cors')
+const app = express();
+
+// Middlewares
+app.use(express.json()) // Permite recibir Jsons desde el Front
+app.use(cors()) // Solucionar el error de Cors al mandar una petición desde el Front
+
+// Routes
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(process.env.PORT | port, () => console.log(`Example app listening at http://localhost:${process.env.PORT | port}`))
+// Server Port
+app.listen(process.env.PORT || 5000, () => {
+    console.log("El server escucha el puerto 3001")
+})
