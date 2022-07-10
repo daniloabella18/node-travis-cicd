e2d3e23c578d53120581b85c76d622a472591af5
daniloabella18 <daniloabella@gmail.com>
2022-07-10 00:43:42 -0400

    *

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-
-const express = require('express')
+const express = require('express');
 const app = express();
+const port = 5000;
 
-// Middlewares
-app.use(express.json()) // Permite recibir Jsons desde el Front
-
-// Routes
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// Server Port
-app.listen(process.env.PORT || 5000, () => {
-    console.log("El server escucha el puerto 3001")
-})
+app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${process.env.PORT || port}`))
